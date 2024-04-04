2cd0c0120b7c21e6e9be2c0519ed6f302b9dd01a
Aida Hashemi <aidahashemiam@gmail.com>
2024-04-05 01:52:42 +0330

    Cut : chart and make gauge chart

--- src/components/DoughnutChart/index.jsx
+++ src/components/DoughnutChart/index.jsx
@@ -21,14 +21,10 @@ function DoughnutChart() {
           datasets: [
             {
               label: "$ ",
-              data: [20, 15, 30, 25, 10],
-              backgroundColor: [
-                "#ffaa35",
-                "#3d3e52",
-                "transparent",
-                "#674aff",
-                "#02c39a",
-              ],
+              data: [20, 15, 25, 10],
+              backgroundColor: ["#674aff", "#02c39a", "#ffaa35", "#3d3e52"],
+              circumference: 240,
+              rotation: -120,
             },
           ],
         }}
