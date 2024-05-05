d8599dc5233109af05ec9bfcb3c85c6e9ccae339
azam <azamjs1990@gmail.com>
2024-05-05 03:57:50 -0700

    create context

--- src/components/MarketPrices/index.jsx
+++ src/components/MarketPrices/index.jsx
@@ -1,35 +1,10 @@
 import MarketPriceItem from "./MarketPriceItem";
 import styles from "./style.module.css";
-import { useContext, useState, useEffect } from "react";
-import { httpClient } from "../../services/http";
-// import { PriceContext } from "../../context/PriceProvider";
-// import { ChartDataProvider } from "../../context/ChartDataProvider";
+
 function MarketPricees() {
-  const [post , setPost]=useState(null)
-  const [error , setError]=useState(null)
-  useEffect(() => {
-    httpClient
-      .get("coins/markets?vs_currency=usd")
-      .then((response) => {
-        setPost(response);
-      })
-      .catch((error) => {
-        setError(error);
-      });
-  }, []);
-  if (error) return `Error: ${error.message}`;
-  // console.log(post)
- 
   return (
-    <div className={styles.marketPrices}>
-      {/* {post?.map((item , index , array) => { */}
-      {/* // return( */}
-        
+    <div className={styles.marketPrices}> 
         <MarketPriceItem  />
-      {/* // ) */}
-       
-   
-      {/* // })} */}
     </div>
   );
 }
