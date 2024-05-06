b57807c37811ca9e90b828a7544b2cc98ac8c012
Aida Hashemi <aidahashemiam@gmail.com>
2024-05-06 15:59:28 +0330

    Use : convertNumber function for market_cap and total_volume numbers

--- src/components/PriceList/List/index.jsx
+++ src/components/PriceList/List/index.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import { convertNumber } from "../../../functions/convertNumber";
 
-// import styles from "./styles.module.css";
+import styles from "./styles.module.css";
 
 const List = ({ coin }) => {
-  return <div>{coin.id}</div>;
+  return (
+    <div className={styles.list_item_container}>
+      <img src={coin.image} alt="crypto" />
+      <p>
+        <span>{coin.name}</span>
+        <span>{coin.symbol}</span>
+      </p>
+      <span>$ {coin.current_price.toLocaleString()}</span>
+      <span>{coin.price_change_percentage_24h.toFixed(2)} %</span>
+      <span>{convertNumber(coin.market_cap)}</span>
+      <span>{convertNumber(coin.total_volume)}</span>
+      <span>{coin.circulating_supply}</span>
+    </div>
+  );
 };
 
 export default List;
