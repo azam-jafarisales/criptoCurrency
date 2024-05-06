27b88a693ac81a9b8f22daccc7a8efa029891180
Aida Hashemi <aidahashemiam@gmail.com>
2024-05-06 17:38:45 +0330

    Get : data for List component and add some styles for them

--- src/components/PriceList/List/index.jsx
+++ src/components/PriceList/List/index.jsx
@@ -1,3 +1,4 @@
+// import { useState } from "react";
 import { convertNumber } from "../../../functions/convertNumber";
 
 import styles from "./styles.module.css";
@@ -5,16 +6,20 @@ import styles from "./styles.module.css";
 const List = ({ coin }) => {
   return (
     <div className={styles.list_item_container}>
+      <span>{coin.market_cap_rank}</span>
       <img src={coin.image} alt="crypto" />
-      <p>
+      <p className={styles.crypto_name}>
         <span>{coin.name}</span>
-        <span>{coin.symbol}</span>
+        <span>{coin.symbol.toUpperCase()}</span>
       </p>
       <span>$ {coin.current_price.toLocaleString()}</span>
       <span>{coin.price_change_percentage_24h.toFixed(2)} %</span>
-      <span>{convertNumber(coin.market_cap)}</span>
-      <span>{convertNumber(coin.total_volume)}</span>
-      <span>{coin.circulating_supply}</span>
+      <span>$ {convertNumber(coin.market_cap)}</span>
+      <span>$ {convertNumber(coin.total_volume)}</span>
+      <div className={styles.circulating_supply}>
+        <span>{coin.circulating_supply.toLocaleString()}</span>
+        <span>{coin.symbol.toUpperCase()}</span>
+      </div>
     </div>
   );
 };
