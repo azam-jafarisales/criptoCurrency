03909c79ef70338a321f25c7883bc916228d40d1
Aida Hashemi <aidahashemiam@gmail.com>
2024-05-16 00:09:19 +0330

    Add : coinData page for each coin .and aslo add some functions in function folder

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -4,6 +4,7 @@ import Prices from "./Prices";
 import TrendingCoins from "./TrendingCoins";
 import Currency from "./Currency";
 import HomePage from "./HomePage";
+import CoinData from "./CoinData";
 
 export const routes = [
   {
@@ -15,6 +16,7 @@ export const routes = [
         element: <HomePage />,
       },
       { path: paths.PRICES, element: <Prices /> },
+      { path: paths.COINDATA, element: <CoinData /> },
       {
         path: paths.TRENDING,
         element: <TrendingCoins />,
